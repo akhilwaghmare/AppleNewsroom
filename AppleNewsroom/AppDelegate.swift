b3d98d8a9c9163065ba3ecf2151f9d13ae079f01
Akhil Waghmare <akhilwaghmare@gmail.com>
2019-06-03 10:03:28 -0400

    Set up split view controller

--- AppleNewsroom/AppDelegate.swift
+++ AppleNewsroom/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let feedListVC = FeedListViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        
+        let splitVC = UISplitViewController()
+        splitVC.viewControllers = [UINavigationController(rootViewController: feedListVC), ViewController()]
+        window?.rootViewController = splitVC
+        
         return true
     }
 
---
+++ AppleNewsroom/FeedListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedListViewController.swift
+//  AppleNewsroom
+//
+//  Created by Akhil Waghmare on 6/3/19.
+//  Copyright © 2019 Akhil Waghmare. All rights reserved.
+//
+
+import UIKit
+
+class FeedListViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+    }
+}
